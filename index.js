b7177d2db7cfedb492ff67996ea247a450e35377
Aldanis Vigo <aldanisvigo@gmail.com>
2022-09-12 23:15:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // const scores = [3,4,21,36,10,28,35,5,24,42]; //Expected 4 0
 const scores = [10,5,20,20,4,5,2,25,1] //Expected 2 4
 
-function breakingRecords(scores) {
+const breakingRecords = (scores) => {
     let currentRecord = null
     let lowestRecord = null
     let leastPointsBroken = 0
